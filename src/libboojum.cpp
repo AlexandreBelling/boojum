b90e9e609f2d8372b3ed56af6d31b43bc7974d6e
Alexandre Belling <alexandre.belling@consensys.net>
2019-02-24 12:17:45 +0100

    feature(clean): Removes debuggings printf

--- src/libboojum.cpp
+++ src/libboojum.cpp
@@ -41,14 +41,9 @@ void make_example_proof(
     void** tree_buffer
 ) {
     unsigned char ** tree_buffer_char = reinterpret_cast<unsigned char **>(tree_buffer);
-    //memfreetree(*tree_buffer_char);
+    
     auto new_value = tree<dual_basT, 0>::as_example(50).to_string();
     tree_buffer_char[0] = new_value;
-
-    for(int q=0; q<16; q++)
-    {
-        printf("%x %x %x\n", tree_buffer, tree_buffer_char[0], tree_buffer_char[0][q]);
-    }
 
     std::vector<std::uint32_t> header = parse_header(*tree_buffer_char);
 }
@@ -81,16 +76,8 @@ void prove_aggregation(
     unsigned char *right_node_char = reinterpret_cast<unsigned char *>(right_node_buff);
     unsigned char **output_node_char = reinterpret_cast<unsigned char **>(output_node_buff);
 
-    //memfreetree(*output_node_char);
-
     std::vector<std::uint32_t> l_header = parse_header(left_node_char);
     std::vector<std::uint32_t> r_header = parse_header(right_node_char);
-
-    printf("Serialized header\n");
-    for(int q=0; q<16; q++)
-    {
-        printf("%x %x \n", right_node_char, right_node_char[q]);
-    }
 
     if(l_header[2] != r_header[2])
     {
